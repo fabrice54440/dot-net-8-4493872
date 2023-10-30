1e44fdde6f8530003f3ffb29fbfe61ae4195de3c
Labasse <labasse@gmail.com>
2023-10-30 16:20:51 +0000

    Naviguer dans les tableaux (début)

---
+++ src/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Tableaux;
+
+class Program
+{
+    static void Afficher(string[] liste)
+    {
+        foreach (var c in liste)
+        {
+            Console.WriteLine(c);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        var couleurs = new string[] {
+            "rouge", "orange", "jaune", "vert", "bleu", "violet"
+        };
+
+        Afficher(couleurs);
+        Console.WriteLine();
+
+        #region Boucle d'initialisation
+        var couleursMaj = new string[couleurs.Length];
+
+        for (int i = 0; i < couleurs.Length; i++)
+        {
+            couleursMaj[i] = couleurs[i].ToUpper();
+        }
+        #endregion
+
+    }
+}

c72f858aa0f89157e4325a50b412f905ae82e603
Labasse <labasse@gmail.com>
2023-10-31 09:52:17 +0000

    Manier les expressions régulières (début)

---
+++ src/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ExpReg;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var bic = "ATCICIAB";
+
+        Console.Write($"Le BIC '{bic}' est ");
+        if (bic is not null) // TODO : [A-Z]{6}[A-Z0-9]{2,5}
+        {
+            Console.WriteLine("valide.");
+        }
+        else
+        {
+            Console.WriteLine("non valide.");
+        }
+
+        var twitGeorgeSand = "Tu fuyais la #solitude et la trouvait #partout.";
+
+        var hashtags = Array.Empty<string>(); // TODO : Extraire #\w+
+
+        foreach (var tag in hashtags)
+        {
+            Console.WriteLine(tag);
+        }
+
+        var html = twitGeorgeSand;
+
+        Console.WriteLine(html);
+    }
+}

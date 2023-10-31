05b8ce240e45b94ff2e6c355b74566b355f71da3
Labasse <labasse@gmail.com>
2023-10-31 09:54:33 +0000

    Manier les expressions régulières (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,17 @@
-﻿namespace ExpReg;
+﻿using System.Text.RegularExpressions;
 
-class Program
+namespace ExpReg;
+
+partial class Program
 {
+    [GeneratedRegex(@"#\w+")]
+    private static partial Regex HashTagRegEx();
     static void Main(string[] args)
     {
         var bic = "ATCICIAB";
 
         Console.Write($"Le BIC '{bic}' est ");
-        if (bic is not null) // TODO : [A-Z]{6}[A-Z0-9]{2,5}
+        if (Regex.IsMatch(input: bic, pattern: "[A-Z]{6}[A-Z0-9]{2,5}"))
         {
             Console.WriteLine("valide.");
         }
@@ -18,14 +22,17 @@ class Program
 
         var twitGeorgeSand = "Tu fuyais la #solitude et la trouvait #partout.";
 
-        var hashtags = Array.Empty<string>(); // TODO : Extraire #\w+
+        var hashtags = HashTagRegEx().Matches(twitGeorgeSand);
 
         foreach (var tag in hashtags)
         {
             Console.WriteLine(tag);
         }
 
-        var html = twitGeorgeSand;
+        var html = HashTagRegEx().Replace(
+          twitGeorgeSand,
+          m => $"<a href='?tag={m.Value.Substring(1)}'>{m.Value}</a>"
+        );
 
         Console.WriteLine(html);
     }

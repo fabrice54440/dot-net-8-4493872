7427d4b41baaa51964d87417ef859f213ce1f35f
Labasse <labasse@gmail.com>
2023-10-31 09:49:20 +0000

    S'adapter à la culture (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,28 +1,34 @@
-﻿namespace Culture;
+﻿using System.Globalization;
+
+namespace Culture;
 
 class Program
 {
     static void Main(string[] args)
     {
+        var culture = CultureInfo.CreateSpecificCulture("fr-FR");
+        Thread.CurrentThread.CurrentCulture = culture;
+
         // Nombres
-      var prix = 3.4m;
+        var prix = 3.4m;
 
-      Console.WriteLine($"{prix} / {prix:c} / {prix:'€ '0.00}");
+        Console.WriteLine($"{prix} / {prix:c} / {prix:'€ '0.00}");
 
-      // Dates
-      var début = new DateTimeOffset(year: 2021, month: 08, day: 10, hour: 12, 0, 0, offset: TimeSpan.FromHours(1));
-      var durée = new TimeSpan(hours: 1, minutes: 30, 0);
-      var fin = début + durée;
-      var attente = début - DateTimeOffset.Now;
+        // Dates
+        var début = new DateTimeOffset(year: 2021, month: 08, day: 10, hour: 12, 0, 0, offset: TimeSpan.FromHours(1));
+        var durée = new TimeSpan(hours: 1, minutes: 30, 0);
+        var fin = début + durée;
+        var attente = début - DateTimeOffset.Now;
 
-      Console.WriteLine($"RDV {début:f} - {fin:F} dans {attente:dd\\jhh\\hmm}");
-      Console.WriteLine($"RDV {début:g} - {fin:G} dans {attente:g}");
-      Console.WriteLine($"RDV le {début:d} à {début:t} jusqu'à {fin:T} dans {attente:G}"); var texte0 = "Quel bel ét\u00e9 !";
+        Console.WriteLine($"RDV {début:f} - {fin:F} dans {attente:dd\\jhh\\hmm}");
+        Console.WriteLine($"RDV {début:g} - {fin:G} dans {attente:g}");
+        Console.WriteLine($"RDV le {début:d} à {début:t} jusqu'à {fin:T} dans {attente:G}");
 
-      // Textes
-      var texte1 = "Quel bel ét\u0065\u0301 !";
-      var comparaison = texte0.Equals(texte1, StringComparison.Ordinal) ? "identiques" : "différents";
+        // Textes
+        var texte0 = "Quel bel ét\u00e9 !";
+        var texte1 = "Quel bel ét\u0065\u0301 !";
+        var comparaison = texte0.Equals(texte1, StringComparison.CurrentCulture) ? "identiques" : "différents";
 
-      Console.WriteLine($"{texte0} et {texte1} sont {comparaison}");
+        Console.WriteLine($"{texte0} et {texte1} sont {comparaison}");
     }
 }

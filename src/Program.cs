ef0a89c1c90df0d3adf06aa4e3275a4f2c3223c8
Labasse <labasse@gmail.com>
2023-10-31 15:18:57 +0000

    Déclencher des actions périodiques (début)

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Periodique;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CompteARebours compteAReboursA = new(5);
+
+        // compteAReboursA.Fini += (o, e) => Console.WriteLine("Compte à rebours terminé");
+
+        compteAReboursA.Tic(); Thread.Sleep(1_000);
+        compteAReboursA.Tic(); Thread.Sleep(1_000);
+        compteAReboursA.Tic(); Thread.Sleep(1_000);
+        compteAReboursA.Tic(); Thread.Sleep(1_000);
+        compteAReboursA.Tic();
+    }
+}

52e863cbe118dc6254e686cd5195a2c98db4898a
Labasse <labasse@gmail.com>
2023-11-02 07:59:15 +0000

    Compresser les fichiers (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.IO.Compression;
+using System.Linq;
 
 namespace Compression;
 
@@ -14,7 +16,28 @@ class Program
         }
 
         // Création d'une archive
+        ZipFile.CreateFromDirectory("./bin", archive.FullName);
+
+        ZipFile.ExtractToDirectory(archive.FullName, "./obj", overwriteFiles: true);
 
         // Ajout d'un fichier
+        using (FileStream zip = new(archive.FullName, FileMode.Open))
+        using (ZipArchive flux = new(zip, ZipArchiveMode.Update))
+        {
+            var liste = flux.Entries.ToArray();
+            var index = flux.CreateEntry("index.txt");
+
+            using (StreamWriter writer = new(index.Open()))
+            {
+                foreach (var fichier in liste)
+                {
+                    writer.WriteLine(
+                        "{0} {1:0.00}%",
+                        fichier.FullName,
+                        (double)fichier.CompressedLength / (double)fichier.Length
+                    );
+                }
+            }
+        }
     }
 }

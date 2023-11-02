fd0a5412e1f0ec5815c6e7c17c45e75db6d163fd
Labasse <labasse@gmail.com>
2023-11-02 09:38:30 +0000

    Envoyer des e-mails (début)

---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Email;
+
+// Ajouter dans launch.json :
+// , "env": {
+//     "SMTP_USER": "",
+//     "SMTP_PASS": "",
+//     "SMTP_HOST": "",
+//     "SMTP_PORT": "",
+//     "SMTP_SSL": "true"/"false"
+// }
+class Program
+{
+    static void Main(string[] args)
+    {
+        var conf = "USER,PASS,HOST,PORT,SSL".Split(',').ToDictionary(
+            p => p, p => Environment.GetEnvironmentVariable($"SMTP_{p}")
+        );
+
+    }
+}

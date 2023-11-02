292536d1f063cdcfe751dc299b73c1fd40237e08
Labasse <labasse@gmail.com>
2023-11-02 10:41:58 +0000

    Interopérer entre processus (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,43 +1,46 @@
-﻿namespace Ipc;
+﻿using System.IO.Pipes;
+
+namespace Ipc;
 
 class Program
 {
     const int EOT = int.MinValue;
 #if TEST
-    // Compil. dans src : dotnet build /p:DefineConstants="TEST" -o test
+        // Compil. dans src : dotnet build /p:DefineConstants="TEST" -o test
     // Codespaces exec : ./test/Ipc
-    static void Main(string[] args)
-    {
-        Span<byte> mem = new(new byte[4]);
-        
-        // Création / Connexion au canal
-        
-        tuyau.Connect();
-                    
-        for(var fib = (n1: 0, n2: 1); fib.n1 < 1000; fib = (fib.n2, fib.n1+fib.n2))
+        static void Main(string[] args)
         {
-            BitConverter.TryWriteBytes(mem, fib.n1);
-            // Envoi de mem dans le canal
+            Span<byte> mem = new(new byte[4]);
+            
+            using NamedPipeClientStream tuyau = new NamedPipeClientStream("traitement-entiers");
+            
+            tuyau.Connect();
+                       
+            for(var fib = (n1: 0, n2: 1); fib.n1 < 1000; fib = (fib.n2, fib.n1+fib.n2))
+            {
+                BitConverter.TryWriteBytes(mem, fib.n1);
+                tuyau.Write(mem);
+            }
+            BitConverter.TryWriteBytes(mem, EOT);
+            tuyau.Write(mem);
+            tuyau.Flush();
+            tuyau.Close();
         }
-        BitConverter.TryWriteBytes(mem, EOT);
-        // Envoi de mem dans le canal
-        // Fermeture
-    }
 #else
     // Codespaces exec : dotnet run
     static void Main(string[] args)
     {
-        // Création du canal
+        using NamedPipeServerStream tuyau = new("traitement-entiers", PipeDirection.In);
         byte[] mem = new byte[sizeof(int)];
 
-        // Attente d'une connexion
+        tuyau.WaitForConnection();
         for (; ; )
         {
-            // Lecture sur le canal
+            tuyau.Read(mem, 0, mem.Length);
             var val = BitConverter.ToInt32(mem);
             if (val == EOT)
             {
-                // Déconnexion
+                tuyau.Disconnect();
                 break;
             }
             Console.WriteLine(val);

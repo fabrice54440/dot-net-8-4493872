0952ca792a23cf2c4379798535fced65dc1ec299
Labasse <labasse@gmail.com>
2023-11-02 07:56:50 +0000

    Compresser les fichiers (début)

---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Compression;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FileInfo archive = new FileInfo("binaires.zip");
+
+        if (archive.Exists)
+        {
+            archive.Delete();
+        }
+
+        // Création d'une archive
+
+        // Ajout d'un fichier
+    }
+}

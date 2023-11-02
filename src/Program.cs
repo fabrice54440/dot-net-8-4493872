fb4fd104224d21c6d6eada149feaa138f8330605
Labasse <labasse@gmail.com>
2023-11-02 09:50:02 +0000

    Explorer le réseau (début)

---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Reseau;
+
+class Program
+{
+    public static void AfficherAdresses(string titre, IEnumerable<IPAddress> adresses)
+    {
+        Console.WriteLine(titre);
+        foreach (var adr in adresses)
+        {
+            Console.WriteLine($"\t- {adr}");
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}

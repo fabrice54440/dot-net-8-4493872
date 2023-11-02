2624299885f442a6692cc1f21dd1edf21defab80
Labasse <labasse@gmail.com>
2023-11-02 10:13:53 +0000

    Proposer un serveur (fin)

--- src/Srv/Program.cs
+++ src/Srv/Program.cs
@@ -1,42 +1,52 @@
 ﻿using System.Collections.Concurrent;
 using System.IO;
+using System.IO;
+using System.Net;
 using System.Net.Sockets;
 
 namespace Srv;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    ConcurrentBag<string> sac = new();
+    TcpListener serveur = new(IPAddress.Any, 10_000);
+    TaskFactory clients = new();
+
+    serveur.Start();
+    for (; ; )
     {
-        ConcurrentBag<string> sac = new();
+      var tcpClient = serveur.AcceptTcpClient();
 
-
+      clients.StartNew(o => Servir((TcpClient)o!, sac), tcpClient);
     }
-    static async void Servir(TcpClient tcp, ConcurrentBag<string> sac)
+  }
+  static async void Servir(TcpClient tcp, ConcurrentBag<string> sac)
+  {
+    try
     {
-      try
-      {
-        using StreamReader r = new(tcp.GetStream());
-        var msg = await r.ReadLineAsync();
+      using StreamReader r = new(tcp.GetStream());
+      var msg = await r.ReadLineAsync();
 
-        Console.WriteLine($"Reçu : {msg}");
+      Console.WriteLine($"Reçu : {msg}");
 
-        using StreamWriter w = new(tcp.GetStream());
+      using StreamWriter w = new(tcp.GetStream());
 
-        foreach (var txt in sac)
-        {
-          await w.WriteLineAsync(txt.AsMemory());
-        }
-        await w.FlushAsync();
-        sac.Add($"{tcp.Client.RemoteEndPoint}: {msg}");
-      }
-      catch (Exception e)
+      foreach (var txt in sac)
       {
-        Console.Error.WriteLine($"{tcp.Client.RemoteEndPoint}: {e.Message}");
+        await w.WriteLineAsync(txt.AsMemory());
       }
-      finally
-      {
-        tcp.Dispose();
-      }
+      await w.FlushAsync();
+      sac.Add($"{tcp.Client.RemoteEndPoint}: {msg}");
     }
+    catch (Exception e)
+    {
+      Console.Error.WriteLine($"{tcp.Client.RemoteEndPoint}: {e.Message}");
+    }
+    finally
+    {
+      tcp.Dispose();
+    }
+  }
 }

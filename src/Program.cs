c34f0e48b4ad4ca8df665f02ccb33d346fb40bd9
Labasse <labasse@gmail.com>
2023-11-02 07:51:21 +0000

    Lire et écrire des fichiers (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace LireEcrire;
+﻿using System.IO;
+using System.Text;
+
+namespace LireEcrire;
 
 class Program
 {
@@ -11,9 +14,25 @@ class Program
 
         try
         {
+            using FileStream flux0 = new(
+                "exemple.dat",
+                FileMode.OpenOrCreate,
+                FileAccess.Write,
+                FileShare.None
+            );
+            using BufferedStream flux1 = new(flux0);
+            using BinaryWriter writer = new(flux1);
 
+            writer.Write(Encoding.ASCII.GetBytes(prénom.PadRight(50)));
+            writer.Write(Encoding.ASCII.GetBytes(nom.PadRight(50)));
+            writer.Write(valeur1);
+            writer.Write(valeur2);
+            writer.Seek(50, SeekOrigin.Begin);
+            writer.Write(Encoding.ASCII.GetBytes("-------".PadRight(50)));
+
+            flux1.Close();
         }
-        catch (Exception e)
+        catch (IOException e)
         {
             Console.Error.WriteLine(e.Message);
         }

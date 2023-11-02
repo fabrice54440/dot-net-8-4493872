164060d1f3f69c287ea0bcb5a8485da2434601a8
Labasse <labasse@gmail.com>
2023-11-02 12:36:43 +0000

    Explorer le code par réflexion (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,18 +1,27 @@
 ﻿namespace Reflexion;
 
 class Program
-  {
+{
     static void Main(string[] args)
     {
-      Produit p = new("Tongs", 2.30m);
+        Produit p = new("Tongs", 2.30m);
 
-      foreach (var prop in typeof(Produit).GetProperties())
-      {
-        Console.WriteLine(prop.Name);
-      }
+        Console.WriteLine(p.GetType().Assembly);
+        Console.WriteLine(p.GetType().Name);
+
+        foreach (var prop in typeof(Produit).GetProperties())
+        {
+            var infos = prop.GetCustomAttributes(typeof(InfosAttribute), false) as InfosAttribute[] ?? Array.Empty<InfosAttribute>();
+
+            Console.Write(infos.Length > 0 ? infos[0].Nom : prop.Name);
+            Console.Write(" : ");
+            Console.WriteLine(prop.GetGetMethod()?.Invoke(p, Array.Empty<object>()));
+        }
     }
-  }
-  public record Produit(string Nom, decimal Prix)
-  {
+}
+[Infos("C'est un produit")]
+public record Produit(string Nom, decimal Prix)
+{
+    [Infos("Le prix TTC")]
     public decimal PrixTTC => Prix * 1.2m;
-  }
+}

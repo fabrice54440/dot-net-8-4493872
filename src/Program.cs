418fcc78d99a9e1dbf373fb8eb9fbfc86024d119
Labasse <labasse@gmail.com>
2023-11-02 09:03:02 +0000

    Consommer une API Rest (début)

---
+++ src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+
+namespace Api;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using HttpClient http = new() { BaseAddress = new Uri("https://petstore.swagger.io/v2/") };
+
+        // Animaux vendus
+        var vendus = http.GetAsync("pet/findByStatus?status=sold");
+
+        Console.WriteLine(vendus.Result.Content.ReadAsStringAsync().Result);
+
+        // Animaux disponibles
+        var disponibles = http.GetAsync("pet/findByStatus?status=available");
+
+        Console.WriteLine(disponibles.Result.Content.ReadAsStringAsync().Result);
+    }
+}

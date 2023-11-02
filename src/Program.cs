b6c4bb0dbbe05d073263ee9b04b8622b990e3dbb
Labasse <labasse@gmail.com>
2023-11-02 09:03:43 +0000

    Consommer une API Rest (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,21 +1,25 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
 namespace Api;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         using HttpClient http = new() { BaseAddress = new Uri("https://petstore.swagger.io/v2/") };
 
-        // Animaux vendus
-        var vendus = http.GetAsync("pet/findByStatus?status=sold");
+        var vendus = http.GetFromJsonAsync<IEnumerable<Pet>>("pet/findByStatus?status=sold"); // Animaux vendus
+        var disponibles = http.GetFromJsonAsync<IEnumerable<Pet>>("pet/findByStatus?status=available"); // Animaux disponibles
 
-        Console.WriteLine(vendus.Result.Content.ReadAsStringAsync().Result);
-
-        // Animaux disponibles
-        var disponibles = http.GetAsync("pet/findByStatus?status=available");
-
-        Console.WriteLine(disponibles.Result.Content.ReadAsStringAsync().Result);
+        foreach (var p in (await vendus)!.Union((await disponibles)!))
+        {
+            Console.WriteLine(p);
+        }
     }
+
+    public record Pet(long Id, string Name, string[] PhotoUrls, string Status);
 }

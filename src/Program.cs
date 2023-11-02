edd73288808f2edcd37ab93730626e52efb7e002
Labasse <labasse@gmail.com>
2023-11-02 08:05:37 +0000

    Sécuriser par chiffrement (fin)

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Chiffrement;
@@ -8,21 +9,50 @@ class Program
     static void Main(string[] args)
     {
         // Symétrique
-        using (FileStream f = new("obj/fichier.sec", FileMode.Create))
-        using (StreamWriter writer = new(f))
+        string key, iv;
+
+        using (var aes = Aes.Create())
         {
-            writer.WriteLine("Le roi est nu");
+            key = Convert.ToBase64String(aes.Key);
+            iv = Convert.ToBase64String(aes.IV);
+            using (FileStream f = new("obj/fichier.sec", FileMode.Create))
+            using (var transform = aes.CreateEncryptor())
+            using (CryptoStream dest = new(f, transform, CryptoStreamMode.Write))
+            using (StreamWriter writer = new(dest))
+            {
+                writer.WriteLine("Le roi est nu");
+            }
         }
 
-        using (FileStream f = new("obj/fichier.sec", FileMode.Open))
-        using (StreamReader reader = new(f))
+        using (var aes = Aes.Create())
         {
-            Console.WriteLine(reader.ReadToEnd());
+            aes.Key = Convert.FromBase64String(key);
+            aes.IV = Convert.FromBase64String(iv);
+            using (FileStream f = new("obj/fichier.sec", FileMode.Open))
+            using (var transform = aes.CreateDecryptor())
+            using (CryptoStream source = new(f, transform, CryptoStreamMode.Read))
+            using (StreamReader reader = new(source))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+            }
         }
 
         // Asymétrique
         var message = Encoding.ASCII.GetBytes("Le roi est nu");
+        byte[] chiffré;
 
-        Console.WriteLine(Encoding.ASCII.GetString(message));
+        using (RSACryptoServiceProvider rsaDest = new())
+        {
+            var cléPublique = rsaDest.ToXmlString(includePrivateParameters: false);
+
+            using (RSACryptoServiceProvider rsaEnvoi = new())
+            {
+                rsaEnvoi.FromXmlString(cléPublique);
+                chiffré = rsaEnvoi.Encrypt(message, false);
+            }
+
+            var octets = rsaDest.Decrypt(chiffré, false);
+            Console.WriteLine(Encoding.ASCII.GetString(octets));
+        }
     }
 }

2f783b99206fa6742177ad18390603459e89922d
fabrice54440 <139238619+fabrice54440@users.noreply.github.com>
2024-06-17 07:51:37 +0000

    CHANGE video 02-01

--- src/Program.cs
+++ src/Program.cs
@@ -5,8 +5,11 @@ class Program
     static void Main(string[] args)
     {
         #region Constantes
-        var fichier = "c:\\exemple.txt";
-        var adresse = "Rue de l'Hôtel-de-Ville\n1200 Genève";
+        var fichier = @"c:\exemple.txt";
+        var adresse = """
+            Rue de l'Hôtel-de-Ville
+            1200 Genève
+            """;
 
         Console.WriteLine(fichier);
         Console.WriteLine(adresse);
@@ -15,26 +18,26 @@ class Program
         #region Mise en forme
         var prix = 9.5m;
 
-        // Console.WriteLine("Le montant est " + prix + " EUR.");
+        Console.WriteLine($"Le montant est {prix:.00} EUR.");
         #endregion
 
         #region Interprétation "Parsing"
         var saisie = "2.40 CHF";
 
-        if (decimal.TryParse(saisie, out var chf))
+        if (decimal.TryParse(saisie.Substring(0, saisie.IndexOf(' ')), out var chf))
         {
             Console.WriteLine($"Le montant lu est : {chf:.00} CHF");
         }
         else
         {
-            // Console.Error.WriteLine("Impossible d'extraire le montant");
+            Console.Error.WriteLine("Impossible d'extraire le montant");
         }
         #endregion
 
         #region Manipulation/Comparaison
-        if (adresse.ToUpper().Contains("GENÈVE"))
+        if (adresse.Contains("GENÈVE", StringComparison.CurrentCultureIgnoreCase))
         {
-            // Console.WriteLine("Adresse genevoise.");
+            Console.WriteLine("Adresse genevoise.");
         }
         else
         {
